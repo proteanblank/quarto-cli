dd9551ca62c7e69bd5db8b489891ae3b8f5ad906
Charles Teague <cteague@gmail.com>
2022-01-21 13:53:03 -0500

    Improve Listing Sort Control

--- src/config/types.ts
+++ src/config/types.ts
@@ -107,6 +107,7 @@ import {
   kListingPageOrderBy,
   kListingPageOrderByDateAsc,
   kListingPageOrderByDateDesc,
+  kListingPageOrderByDefault,
   kListingPageOrderByNumberAsc,
   kListingPageOrderByNumberDesc,
   kListings,
@@ -477,6 +478,7 @@ export interface FormatLanguage {
   [kCrossrefLolTitle]?: string;
   [kListingPageOrderBy]?: string;
   [kListingPageOrderByDateAsc]?: string;
+  [kListingPageOrderByDefault]?: string;
   [kListingPageOrderByDateDesc]?: string;
   [kListingPageOrderByNumberAsc]?: string;
   [kListingPageOrderByNumberDesc]?: string;
